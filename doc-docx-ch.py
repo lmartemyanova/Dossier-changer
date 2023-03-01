a9fb95818724a291a5ffa4d4c6d72659a21ec626
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-03-02 00:13:54 +0300

    changed library for doc-docx-ch.py

--- doc-docx-ch.py
+++ doc-docx-ch.py
@@ -1,19 +1,14 @@
-# pip install -U pypiwin32
-from win32com import client as wc
-
+import aspose.words as aw
 import os
-w = wc.Dispatch('Word.Application')
 
 paths = []
 folder = os.getcwd()
+
 for root, dirs, files in os.walk(folder):
     for file in files:
         if file.endswith('doc') and not file.startswith('~'):
             paths.append(os.path.join(root, file))
 
 for path in paths:
-    doc = w.Documents.Open(path)
-    doc.SaveAs(path+"x", 16)
-    doc.Close()
-
-w.Quit()
+    doc = aw.Document(path)
+    doc.save(path+'x')
